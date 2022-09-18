ec1d15a4e06e116dcf4b0128eced8ff62ee76ec1
Tu Hoang <tuhoangz2k@gmail.com>
2022-09-18 16:51:02 +0700

    create gitignore and prettier

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    var a=1
+    var b=2
+    var c=a+b
+  res.send('Hello the World!')
 })
 
 app.listen(port, () => {
